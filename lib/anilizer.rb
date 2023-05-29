8d56f4b9022c590547c822de12158378a9cd2a7c
Astapov1138 <Yakor2026@mail.ru>
2023-05-29 21:40:37 +0300

    WordsFromFileFullFix

--- lib/anilizer.rb
+++ lib/anilizer.rb
@@ -4,7 +4,7 @@ require_relative "anilizer/version"
 
 module Anilizer
   class Error < StandardError; end
-  
+
   def self.read_words_from_file_ignoring_some_words(path_to_dir, arr_of_ignored_words) #arr_of_file_names
     words = Hash.new(0)
     count = 0;
@@ -12,10 +12,10 @@ module Anilizer
     txt_files = Dir.entries(path_to_dir).select { |f| File.file? File.join(path_to_dir, f) }
     txt_files.each do |file|
       File.open(path_to_dir + "\\" + file, "r").each_line do |line|
-        strings = line.split(' ')
+        line = line.delete(".,-:;()!?1234567890\n")
+        strings = line.split(/ /)
         strings = strings.map{|y| y.downcase}
         strings.each do |word|
-          
           if (!arr_ignored.include?(word) && !arr_of_ignored_words.include?(word))
             words[word.downcase] += 1;
           end
