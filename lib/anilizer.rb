02ce1a61f4cf48d9a45f86f9041fd6cd1d40e029
Roman Burdukovskii <burdukovskii@sfedu.ru>
2023-05-31 01:47:49 +0300

    обработка исключения No such file or directory

--- lib/anilizer.rb
+++ lib/anilizer.rb
@@ -29,11 +29,15 @@ module Anilizer
   end
 end
 
-puts "Enter the path to the folder that contains txt files"
-path = gets.chomp
-puts "Enter the words that you don't want to count (dividing by space)"
-restricts = gets.chomp
-arr_restricts = restricts.split(' ')
-puts "There are the results"
-hash = Anilizer.read_words_from_file_ignoring_some_words(path, arr_restricts)
-p hash
+begin
+  puts "Enter the path to the folder that contains txt files"
+  path = gets.chomp
+  puts "Enter the words that you don't want to count (dividing by space)"
+  restricts = gets.chomp
+  arr_restricts = restricts.split(' ')
+  puts "There are the results"
+  hash = Anilizer.read_words_from_file_ignoring_some_words(path, arr_restricts)
+  p hash
+rescue Errno::ENOENT => error
+  puts "Folder not found: " + error.message
+end
