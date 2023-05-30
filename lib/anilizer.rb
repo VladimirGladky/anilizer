ddbbbbfba1d6ac4596c0deb4d7d8795fe5255042
M-Zverev <mzverev03@mail.ru>
2023-05-31 00:29:12 +0300

    Add mystem lemmatization

--- lib/anilizer.rb
+++ lib/anilizer.rb
@@ -4,8 +4,41 @@ require_relative "anilizer/version"
 
 module Anilizer
   class Error < StandardError; end
+<<<<<<< Updated upstream
   
   def self.hello
     'hello'
   end
 end
+=======
+
+  def self.lemmatize_file(file_path, arr_of_ignored_words)
+    words = Hash.new(0)
+    arr_ignored = arr_of_ignored_words.map(&:downcase)
+    output = `mystem.exe #{file_path} -c -l -s -d`
+
+    words_list = output.scan(/[[:alpha:]]+/)
+
+    words_list.each do |word|
+      lemma = word.downcase
+
+      next if arr_ignored.include?(lemma)
+
+      words[lemma] += 1
+    end
+
+    words
+  end
+end
+
+puts "Enter the path to the folder that contains txt files"
+path = gets.chomp
+puts "Enter the words that you don't want to count (dividing by space)"
+restricts = gets.chomp
+arr_restricts = restricts.split(' ')
+puts "There are the results"
+hash = Anilizer::lemmatize_file(path, arr_restricts)
+hash.each do |word, count|
+  puts "Слово: #{word}, Количество: #{count}"
+end
+>>>>>>> Stashed changes
